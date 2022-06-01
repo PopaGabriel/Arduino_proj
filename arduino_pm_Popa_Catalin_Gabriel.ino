610ec512d6a907a6fa163d2947d6b5e2c1ef6cd7
PopaGabriel <catalingabrielpopa847@gmail.com>
2022-06-02 02:20:54 +0300

    First push of refactoring working on the humidity

---
+++ arduino_pm_Popa_Catalin_Gabriel.ino
@@ -0,0 +1,115 @@
+#include <LiquidCrystal_I2C.h>
+#include <Servo.h>
+#include <Wire.h>
+
+// Inti the lcd screen
+LiquidCrystal_I2C lcd(0x27, 17, 2);
+
+// Delay for the debounce and debounce timer
+int last_debounce = 0;
+int debounce_delay = 300;
+
+// The Pin of the Button
+const int buttonPin = 2;
+
+// The Pin of the Button
+const int lightPin = 7;
+// The Pins for the servos
+const int servoPin1 = 8;
+const int servoPin2 = 9;
+
+// Initial state for the button
+int buttonState = 0;
+
+// variable for water
+bool water_present = 1;
+
+// Init the servos
+Servo Servo_water;
+Servo Servo_curtain;
+
+void setup()
+{
+  Serial.begin(9600);
+
+  pinMode(lightPin, INPUT);
+  pinMode(buttonPin, INPUT);
+
+  Servo_water.attach(servoPin1);
+  Servo_water.write(90);
+
+  Servo_curtain.attach(servoPin2);
+  Servo_curtain.write(0);
+
+  lcd.init();
+  lcd.backlight();
+}
+
+// checks temperature and moves the servo
+void check_humidity(int humidity)
+{
+  if (humidity == HIGH)
+  {
+    Servo_water.write(0);
+    water_present = 0;
+  }
+  else
+  {
+    Servo_water.write(90);
+  }
+}
+
+// Checks the light and moves the servos
+void check_light(int light)
+{
+  if (light == HIGH)
+  {
+    Servo_curtain.write(90);
+  }
+  else
+  {
+    Servo_curtain.write(0);
+  }
+}
+
+// Solve the water and light
+void solve_problem_water_light()
+{
+  int light = digitalRead(lightPin);
+  check_light(light);
+
+  delay(300);
+  print_lcd(light);
+}
+
+// print to lcd
+void print_lcd(int light)
+{
+  lcd.setCursor(0, 0);
+  lcd.print("Intensity  ");
+  if (light == HIGH)
+    lcd.print(HIGH);
+  else
+    lcd.print(LOW);
+}
+
+// Checks button state
+void check_button()
+{
+  buttonState = digitalRead(buttonPin);
+  if ((millis() - last_debounce) > debounce_delay)
+  {
+    if (buttonState == HIGH && water_present)
+    {
+      water_present = 0;
+      Servo_water.write(0);
+      delay(300);
+    }
+  }
+}
+
+void loop()
+{
+  solve_problem_water_light();
+  check_button();
+}
